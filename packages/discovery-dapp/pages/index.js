f247411880ad426b4be046438cb8dadee7dd0310
aeither <36173828+aeither@users.noreply.github.com>
2021-08-02 10:40:28 +0200

    Add user sign in

--- packages/discovery-dapp/pages/index.js
+++ packages/discovery-dapp/pages/index.js
@@ -1,10 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
 
 export default function Home() {
-  return (
-    <div>
-      Hello world
-    </div>
-  )
+  const [address, setAddress] = useState();
+  useEffect(() => {
+    async function fetchUser() {
+      const web3Modal = new Web3Modal()
+      const connection = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(connection)
+      const signer = provider.getSigner()
+      const userAddress = await signer.getAddress()
+      setAddress(userAddress)
+      console.log("Account:", userAddress);
+
+      // const provider = new ethers.providers.Web3Provider(
+      //   window.ethereum,
+      //   "any"
+      // );
+      // // Prompt user for account connections
+      // await provider.send("eth_requestAccounts", []);
+      // const signer = provider.getSigner();
+      // console.log("Account:", await signer.getAddress());
+    }
+
+    fetchUser();
+  },[]);
+  return <div>Hello world</div>; {address}
 }
