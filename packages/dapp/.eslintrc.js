2081953313cb8088a3e1694f3653720b75d7d12d
Cali93 <armut.cumaali@gmail.com>
2021-08-04 22:11:10 +0200

    🐙 self.id as a starter kit 🚀

---
+++ packages/dapp/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['3box', '3box/react', '3box/typescript', 'next', 'next/core-web-vitals'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+}
