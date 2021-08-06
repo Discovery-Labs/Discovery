1297ccd5dd8649ba6aefa125c69523d5be48235f
Cali93 <armut.cumaali@gmail.com>
2021-08-07 00:48:37 +0200

    fix schema export

--- packages/schemas/lib/schemas.js
+++ packages/schemas/lib/schemas.js
@@ -3,7 +3,7 @@
 const { CourseSchema } = require('./courses/course-schema');
 const { CoursesSchema } = require('./courses/courses-schema');
 const { ProjectSchema } = require('./projects/project-schema');
-const { ProjectsSchema } = require('./projects-schema');
+const { ProjectsSchema } = require('./projects/projects-schema');
 const { RepoSchema } = require('./repos/repo');
 const { ReposSchema } = require('./repos/repos');
 
