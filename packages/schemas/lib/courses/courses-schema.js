29f2aca0ef75d47cd0fe937f8b856788b9edd398
Cali93 <armut.cumaali@gmail.com>
2021-08-07 00:20:39 +0200

    data model schemas

---
+++ packages/schemas/lib/courses/courses-schema.js
@@ -0,0 +1,32 @@
+export const CoursesSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  title: 'Courses',
+  type: 'object',
+  properties: {
+    projects: {
+      type: 'array',
+      title: 'courses',
+      items: {
+        type: 'object',
+        title: 'Course',
+        properties: {
+          id: {
+            $ref: '#/definitions/CeramicStreamId',
+          },
+          name: {
+            type: 'string',
+            title: 'name',
+            maxLength: 100,
+          },
+        },
+      },
+    },
+  },
+  definitions: {
+    CeramicStreamId: {
+      type: 'string',
+      pattern: '^ceramic://.+(\\\\?version=.+)?',
+      maxLength: 150,
+    },
+  },
+}
---
+++ packages/schemas/lib/repos/repo.js
@@ -0,0 +1,34 @@
+export const RepoSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  title: 'Project',
+  type: 'object',
+  properties: {
+    type: "object",
+    properties: {
+      id: {
+        $ref: "#/components/schemas/CeramicDocId"
+      },
+      repo_url: {
+        type: "string"
+      },
+      title: {
+        type: "string"
+      },
+      description: {
+        type: "string"
+      },
+      files: {
+        type: "array",
+        items: {
+          $ref: "#/components/schemas/CeramicDocId"
+        }
+      },
+      authors: {
+        type: "array",
+        items: {
+          $ref: "#/components/schemas/CeramicDocId"
+        }
+      }
+    }
+  }
+}
