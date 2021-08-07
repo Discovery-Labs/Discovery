8ede080c9cca8c74505fa9cfdc90d0789713987b
Cali93 <armut.cumaali@gmail.com>
2021-08-07 22:20:07 +0200

    learners & contributors schemas

--- packages/schemas/lib/schemas.js
+++ packages/schemas/lib/schemas.js
@@ -1,16 +1,20 @@
 'use strict';
 
+const { Contributors } = require('./contributors/contributors-schema');
 const { CourseSchema } = require('./courses/course-schema');
 const { CoursesSchema } = require('./courses/courses-schema');
+const { Learners } = require('./learners/learners-schema');
 const { ProjectSchema } = require('./projects/project-schema');
 const { ProjectsSchema } = require('./projects/projects-schema');
-const { RepoSchema } = require('./repos/repo');
-const { ReposSchema } = require('./repos/repos');
+const { RepoSchema } = require('./repos/repo-schema');
+const { ReposSchema } = require('./repos/repos-schema');
 
 exports.schemas = {
     discovery: {
         courses: CoursesSchema,
         course: CourseSchema,
+        contributors: Contributors,
+        learners: Learners,
         projects: ProjectsSchema,
         project: ProjectSchema,
         repos: ReposSchema,
